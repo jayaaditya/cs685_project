e0e348fd655abfedb92b08f473635ee3bedea8f9
Jaya Aditya <jayaaditya@outlook.com>
2018-11-14 23:20:47 +0530

    assign regions

--- ass_reg.py
+++ ass_reg.py
@@ -5,5 +5,5 @@ folder = 'newdata/health3/'
 files = os.listdir(folder)
 maindf = pd.read_csv('merged_health.csv', index_col = 0)
 df = pd.read_csv('UNSD-Methodology.csv', index_col = 3)
-maindf = maindf.merge(df[['Region Name']], right_index = True ,left_index = True, how = 'left')
+maindf = maindf.merge(df[['Sub-region Name']], right_index = True ,left_index = True, how = 'left')
 maindf.to_csv('merged_health_reg.csv')
